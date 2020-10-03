914bbadc78bc2ca8003ac2da60900d290f6f0d0f
hafidz <hafidzdev@gmail.com>
2020-10-03 15:50:52 +0700

    remove deprecated tag, and change var to let

--- hourglass.js
+++ hourglass.js
@@ -1,16 +1,13 @@
-function hourglass()	
-{	
-  var a;	
-  a = document.getElementById("div1");	
-  a.innerHTML = "&#xf251;";	
-  setTimeout(function ()	
-  {	
-    a.innerHTML = "&#xf252;";	
-  }, 1000);	
-    setTimeout(function ()	
-  {	
-    a.innerHTML = "&#xf253;";	
-  }, 2000);	
-}	
-hourglass();	
+function hourglass() {
+  let a;
+  a = document.getElementById('div1');
+  a.innerHTML = '&#xf251;';
+  setTimeout(function () {
+    a.innerHTML = '&#xf252;';
+  }, 1000);
+  setTimeout(function () {
+    a.innerHTML = '&#xf253;';
+  }, 2000);
+}
+hourglass();
 setInterval(hourglass, 3000);
